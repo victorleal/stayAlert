18b02899251d4da135867df0b21d33c1c450f52b
Endel Guimaraes Silva <endel.gs@gmail.com>
2015-11-20 16:15:42 -0200

    Cross Validation OK

--- LogR.r
+++ LogR.r
@@ -1,8 +1,8 @@
-print("------------------------------- LOG-R --------------------------------")
-print("")
+cat("------------------------------- LOG-R --------------------------------",sep="\n\n")
 
 model_glm <- glm(IsAlert~.,data = TRAIN, family = binomial())
 predicted_glm <- predict(model_glm, newdata = TEST,type="response")
 tbl <- table(pred = as.factor(ifelse(predicted_glm>.5,1,0)), actual = TEST$IsAlert)
 print(tbl)
-print(paste("Accuracy on LogR:",sum(diag(tbl/length(VALIDATION_SET)))))
+cat(paste("Accuracy on LogR:",sum(diag(tbl/length(VALIDATION_SET)))),sep="\n")
+cat("\n")
--- Stay Alert.r
+++ Stay Alert.r
@@ -2,7 +2,7 @@ library(dplyr)
 library(ROCR)
 
 # VARIAVEIS DE SETUP ============================================
-CLEANUP_P <- TRUE
+CLEANUP_P <- FALSE
 BALANCE <- TRUE
 BAGGING <- TRUE
 NORMALIZE <- TRUE
@@ -17,15 +17,16 @@ fordTrain <- arrange(fordTrain,TrialID, ObsNum)
 
 # REDIRECIONANDO A SAIDA PARA LOG AUTOMATICO
 sink("log.txt", append = T)
-"----------------------- START OF EXPERIMENTS -----------------------"
-""
-
+cat("----------------------- START OF EXPERIMENTS -----------------------")
+cat("\n\n")
+print(tbl)
 Y <- factor(fordTrain$IsAlert)
 
 # Descartando variaveis desnecessárias
 fordTrain$X <- NULL
 fordTrain$V7 <- NULL
 fordTrain$V9 <- NULL
+fordTrain$P8 <- NULL
 fordTrain$TrialID <- NULL
 fordTrain$ObsNum <- NULL
 fordTrain$IsAlert <- NULL
@@ -40,7 +41,6 @@ if(CLEANUP_P){
   fordTrain$P5 <- NULL
   fordTrain$P6 <- NULL
   fordTrain$P7 <- NULL
-  fordTrain$P8 <- NULL
 }else{
   MESSAGE <- "P-VARS: Yes"
 }
@@ -48,60 +48,71 @@ if(CLEANUP_P){
 # TREINAMENTO
 SPLIT <- as.integer(nrow(fordTrain)/2)
 
-# inverso das probabilidades originais do dataset
-if(BALANCE){
-  MESSAGE <- paste(MESSAGE,"BALANCED: Yes",sep=" / ")
-  PROB <- ifelse(Y == 1,.31,.69)
-}else{
-  MESSAGE <- paste(MESSAGE,"BALANCED: No",sep=" / ")
-  PROB <- ifelse(Y == 1,.5,.5) # Nao balanceado
-}
+for(i in 1:2){
+  if(i==1){
+    cat(MESSAGE,"TRAIN A-TEST B",sep="\n")
+    TRAIN_RANGE <- c(1:SPLIT)
+    TEST_RANGE <- c(SPLIT:nrow(fordTrain))
+  }else{
+    cat(MESSAGE,"TRAIN B-TEST A",sep="\n")
+    TEST_RANGE <- c(1:SPLIT)
+    TRAIN_RANGE <- c(SPLIT:nrow(fordTrain))
+  }
+  # Inverso das probabilidades originais do dataset
+  if(BALANCE){
+    MESSAGE <- paste(MESSAGE,"BALANCED: Yes",sep=" / ")
+    p0 <- table(Y[TRAIN_RANGE])[1]/length(TRAIN_RANGE)
+    p1 <- table(Y[TRAIN_RANGE])[2]/length(TRAIN_RANGE)
+    PROB <- ifelse(Y == 1,p0,p1)
+  }else{
+    MESSAGE <- paste(MESSAGE,"BALANCED: No",sep=" / ")
+    PROB <- ifelse(Y == 1,.5,.5) # Nao balanceado
+  }
 
-# Usando a PRIMEIRA METADE pra treinamento
-if(BAGGING){
-  MESSAGE <- paste(MESSAGE,"BOOTSTRAP: Yes",sep=" / ")
-  BOOTSTRAP <- sample(c(1:SPLIT),size = SPLIT, replace = T,prob = PROB[1:SPLIT])
-}else{
-  MESSAGE <- paste(MESSAGE,"BOOTSTRAP: No",sep=" / ")
-  BOOTSTRAP <- c(1:SPLIT)
-}
-TRAIN <- fordTrain[BOOTSTRAP,]
-# Garantindo que nao ha sobreposicao treino-teste
-VALIDATION_SET <- setdiff(c(SPLIT:nrow(fordTrain)),BOOTSTRAP)
-VALIDATION_SET <- sample(VALIDATION_SET, size = SPLIT, replace = F)
-TEST <- fordTrain[VALIDATION_SET,]
+  # Usando a PRIMEIRA METADE pra treinamento
+  if(BAGGING){
+    MESSAGE <- paste(MESSAGE,"BOOTSTRAP: Yes",sep=" / ")
+    BOOTSTRAP <- sample(TRAIN_RANGE,size = SPLIT, replace = T,prob = PROB[TRAIN_RANGE])
+  }else{
+    MESSAGE <- paste(MESSAGE,"BOOTSTRAP: No",sep=" / ")
+    BOOTSTRAP <- c(TRAIN_RANGE)
+  }
+  TRAIN <- fordTrain[BOOTSTRAP,]
+  # Garantindo que nao ha sobreposicao treino-teste
+  VALIDATION_SET <- setdiff(c(TEST_RANGE),BOOTSTRAP)
+  VALIDATION_SET <- sample(VALIDATION_SET, size = as.integer(SPLIT/3*4), replace = T)
+  TEST <- fordTrain[VALIDATION_SET,]
 
 
-# NORMALIZACAO DOS DADOS
-if(NORMALIZE){
-  if(NORM_BY == "COL"){
-    MESSAGE <- paste(MESSAGE,"NORMALIZE: by col",sep=" / ")
-    MEANS <- apply(TRAIN,2,mean)
-    SD <- apply(TRAIN,2,sd)
-    TRAIN <- (TRAIN - MEANS)/SD
-    TEST <- (TEST - MEANS)/SD
-  }else if(NORM_BY == "ROW"){
-    MESSAGE <- paste(MESSAGE,"NORMALIZE: by row",sep=" / ")
-    TRAIN <- as.data.frame(t(apply(TRAIN,1,function(x){(x-mean(x))/sd(x)})))
-    TEST  <- as.data.frame(t(apply(TEST, 1,function(x){(x-mean(x))/sd(x)})))
+  # NORMALIZACAO DOS DADOS
+  if(NORMALIZE){
+    if(NORM_BY == "COL"){
+      MESSAGE <- paste(MESSAGE,"NORMALIZE: by col",sep=" / ")
+      MEANS <- apply(TRAIN,2,mean)
+      SD <- apply(TRAIN,2,sd)
+      TRAIN <- (TRAIN - MEANS)/SD
+      TEST <- (TEST - MEANS)/SD
+    }else if(NORM_BY == "ROW"){
+      MESSAGE <- paste(MESSAGE,"NORMALIZE: by row",sep=" / ")
+      TRAIN <- as.data.frame(t(apply(TRAIN,1,function(x){(x-mean(x))/sd(x)})))
+      TEST  <- as.data.frame(t(apply(TEST, 1,function(x){(x-mean(x))/sd(x)})))
+    }else{
+      MESSAGE <- paste(MESSAGE,"NORMALIZE: No",sep=" / ")
+    }
   }else{
     MESSAGE <- paste(MESSAGE,"NORMALIZE: No",sep=" / ")
   }
-}else{
-  MESSAGE <- paste(MESSAGE,"NORMALIZE: No",sep=" / ")
+
+  TRAIN$IsAlert <- Y[BOOTSTRAP]
+  TEST$IsAlert <- Y[VALIDATION_SET]
+
+  if(i==1) cat(MESSAGE,sep="\n")
+
+  source("LDA.r")
+  source("RandomForests.r")
+  source("LogR.r")
 }
-
-TRAIN$IsAlert <- Y[BOOTSTRAP]
-TEST$IsAlert <- Y[VALIDATION_SET]
-
-MESSAGE
-
-source("LDA.r")
-source("RandomForests.r")
-source("LogR.r")
-
-"------------------------ END OF EXPERIMENTS ------------------------"
-""
-
+cat("------------------------ END OF EXPERIMENTS ------------------------",sep="\n")
+cat("\n")
 sink(NULL)
 gc()
